f2b551aa7904370a456782699335c31a205cac0c
Christophe Laferriere <christophe.laferriere@gmail.com>
2010-07-16 14:46:53 +0200

    Work on the main target

--- blur_adapt_xkaapi/blur.h
+++ blur_adapt_xkaapi/blur.h
@@ -12,16 +12,16 @@
 
 //#define BLUR_DEBUG
 
-/**********************
- * COMPONENTS METHODS *
- **********************/
+// /**********************
+//  * COMPONENTS METHODS *
+//  **********************/
 
-// the application main entrypoint.
-void app_main_body (void* taskarg, kaapi_thread_t* thread);
-// blur method from Work interface.
-void blur_body     (void* taskarg, kaapi_thread_t* thread);
-// signal meth from Collect interf.
-void signal_body   (void* taskarg, kaapi_thread_t* thread);
+// // the application main entrypoint.
+// void app_main_body (void* taskarg, kaapi_thread_t* thread);
+// // blur method from Work interface.
+// void blur_body     (void* taskarg, kaapi_thread_t* thread);
+// // signal meth from Collect interf.
+// void signal_body   (void* taskarg, kaapi_thread_t* thread);
 // set_info m. from Collect interf.
 void set_info_body (void* taskarg, kaapi_thread_t* thread);
 
@@ -67,50 +67,50 @@ typedef struct set_info_arg_t {
   int nb_block;
 } set_info_arg_t;
 
-/*********************
- * TASKS DECLARATION *
- *********************/
+// /*********************
+//  * TASKS DECLARATION *
+//  *********************/
 
-/* app_main task decl. : */
+// /* app_main task decl. : */
 
-KAAPI_REGISTER_TASKFORMAT ( app_main_format,
-			    "app_main",
-			    app_main_body,
-			    sizeof (app_main_arg_t),
-			    2,
-			    (kaapi_access_mode_t[]) {KAAPI_ACCESS_MODE_V, KAAPI_ACCESS_MODE_V},
-			    (kaapi_offset_t[]) {offsetof(app_main_arg_t, argc), offsetof(app_main_arg_t, argv)},
-			    (const struct kaapi_format_t*[]) {kaapi_int_format, kaapi_ulong_format})
+// KAAPI_REGISTER_TASKFORMAT ( app_main_format,
+// 			    "app_main",
+// 			    app_main_body,
+// 			    sizeof (app_main_arg_t),
+// 			    2,
+// 			    (kaapi_access_mode_t[]) {KAAPI_ACCESS_MODE_V, KAAPI_ACCESS_MODE_V},
+// 			    (kaapi_offset_t[]) {offsetof(app_main_arg_t, argc), offsetof(app_main_arg_t, argv)},
+// 			    (const struct kaapi_format_t*[]) {kaapi_int_format, kaapi_ulong_format})
 
-/* blur task decl. : */
+// /* blur task decl. : */
 
-KAAPI_REGISTER_TASKFORMAT ( blur_format,
-			    "blur",
-			    blur_body,
-			    sizeof (blur_arg_t),
-			    6,
-			    (kaapi_access_mode_t[]) {KAAPI_ACCESS_MODE_V, KAAPI_ACCESS_MODE_V, KAAPI_ACCESS_MODE_V, KAAPI_ACCESS_MODE_V, KAAPI_ACCESS_MODE_V, KAAPI_ACCESS_MODE_V},
-			    (kaapi_offset_t[]) {offsetof(blur_arg_t, array), offsetof(blur_arg_t, ysize), offsetof(blur_arg_t, xstart), offsetof(blur_arg_t, ystart), offsetof(blur_arg_t, xblock_size), offsetof(blur_arg_t,yblock_size)},
-			    (const struct kaapi_format_t*[]) {kaapi_ulong_format, kaapi_int_format, kaapi_int_format, kaapi_int_format, kaapi_int_format, kaapi_int_format})
+// KAAPI_REGISTER_TASKFORMAT ( blur_format,
+// 			    "blur",
+// 			    blur_body,
+// 			    sizeof (blur_arg_t),
+// 			    6,
+// 			    (kaapi_access_mode_t[]) {KAAPI_ACCESS_MODE_V, KAAPI_ACCESS_MODE_V, KAAPI_ACCESS_MODE_V, KAAPI_ACCESS_MODE_V, KAAPI_ACCESS_MODE_V, KAAPI_ACCESS_MODE_V},
+// 			    (kaapi_offset_t[]) {offsetof(blur_arg_t, array), offsetof(blur_arg_t, ysize), offsetof(blur_arg_t, xstart), offsetof(blur_arg_t, ystart), offsetof(blur_arg_t, xblock_size), offsetof(blur_arg_t,yblock_size)},
+// 			    (const struct kaapi_format_t*[]) {kaapi_ulong_format, kaapi_int_format, kaapi_int_format, kaapi_int_format, kaapi_int_format, kaapi_int_format})
 
-/* signal task decl. : */
+// /* signal task decl. : */
 
-KAAPI_REGISTER_TASKFORMAT ( signal_format,
-			    "signal",
-			    signal_body,
-			    sizeof (signal_arg_t),
-			    0,
-			    (kaapi_access_mode_t[]) {},
-			    (kaapi_offset_t[]) {},
-			    (const struct kaapi_format_t*[]) {})
+// KAAPI_REGISTER_TASKFORMAT ( signal_format,
+// 			    "signal",
+// 			    signal_body,
+// 			    sizeof (signal_arg_t),
+// 			    0,
+// 			    (kaapi_access_mode_t[]) {},
+// 			    (kaapi_offset_t[]) {},
+// 			    (const struct kaapi_format_t*[]) {})
 
-/* set_info task decl. : */
+// /* set_info task decl. : */
 
-KAAPI_REGISTER_TASKFORMAT ( set_info_format,
-			    "set_info",
-			    set_info_body,
-			    sizeof (set_info_arg_t),
-			    6,
-			    (kaapi_access_mode_t[]) {KAAPI_ACCESS_MODE_V, KAAPI_ACCESS_MODE_V, KAAPI_ACCESS_MODE_V, KAAPI_ACCESS_MODE_V, KAAPI_ACCESS_MODE_V, KAAPI_ACCESS_MODE_V},
-			    (kaapi_offset_t[]) {offsetof(set_info_arg_t, fileout_name), offsetof(set_info_arg_t, xsize), offsetof(set_info_arg_t, ysize), offsetof(set_info_arg_t, maxrgb), offsetof(set_info_arg_t, array), offsetof(set_info_arg_t, nb_block)},
-			    (const struct kaapi_format_t*[]) {kaapi_ulong_format, kaapi_int_format, kaapi_int_format, kaapi_int_format, kaapi_ulong_format, kaapi_int_format})
+// KAAPI_REGISTER_TASKFORMAT ( set_info_format,
+// 			    "set_info",
+// 			    set_info_body,
+// 			    sizeof (set_info_arg_t),
+// 			    6,
+// 			    (kaapi_access_mode_t[]) {KAAPI_ACCESS_MODE_V, KAAPI_ACCESS_MODE_V, KAAPI_ACCESS_MODE_V, KAAPI_ACCESS_MODE_V, KAAPI_ACCESS_MODE_V, KAAPI_ACCESS_MODE_V},
+// 			    (kaapi_offset_t[]) {offsetof(set_info_arg_t, fileout_name), offsetof(set_info_arg_t, xsize), offsetof(set_info_arg_t, ysize), offsetof(set_info_arg_t, maxrgb), offsetof(set_info_arg_t, array), offsetof(set_info_arg_t, nb_block)},
+// 			    (const struct kaapi_format_t*[]) {kaapi_ulong_format, kaapi_int_format, kaapi_int_format, kaapi_int_format, kaapi_ulong_format, kaapi_int_format})
