0d7e14297ff2cc1151ed6fc7ba45f033faf2acb6
Christophe Laferriere <christophe.laferriere@gmail.com>
2010-07-19 10:56:13 +0200

    Added simple mutex lock protection and return success or fail for push pop and steal operation

--- blur_adapt_xkaapi/kaapi_workqueue.h
+++ blur_adapt_xkaapi/kaapi_workqueue.h
@@ -1,7 +1,11 @@
+#include <pthread.h>
+
 #ifndef _KAAPI_WORKQUEUE_H_
 #define _KAAPI_WORKQUEUE_H_
 
 #define STACK_MAX_ELEMENT 512
+
+pthread_mutex_t _internal_mutex;
 
 typedef struct stack
 {
@@ -17,15 +21,18 @@ int stack_size (kaapi_stack_t* my_stack);
 
 int stack_is_empty (kaapi_stack_t* my_stack);
 
-void stack_push (kaapi_stack_t* my_stack, void* element);
+// return 1 if succeed and 0 if fail.
+int stack_push (kaapi_stack_t* my_stack, void* element);
 
 //void stack_push_back (kaapi_stack_t* my_stack, void* element);
 
-void stack_pop (kaapi_stack_t* my_stack, void** element);
+// return 1 if succeed and 0 if fail.
+int stack_pop (kaapi_stack_t* my_stack, void** element);
 
 //void stack_pop_safe (kaapi_stack_t* my_stack, void* element);
 
-void stack_steal (kaapi_stack_t* my_stack, void** element);
+// return 1 if succeed and 0 if fail.
+int stack_steal (kaapi_stack_t* my_stack, void** element);
 
 //void stack_steal_unsafe (kaapi_stack_t* my_stack, void* element);
 
