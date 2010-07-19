6d78c360c48de3b7f6e704d8bdc143e15cbb573f
Christophe Laferriere <christophe.laferriere@gmail.com>
2010-07-19 12:48:44 +0200

    Add libppm to the cleaning script

--- cleaning.sh
+++ cleaning.sh
@@ -2,6 +2,10 @@
 
 for dir in `ls blur* -d`; do
   cd $dir;
-  make clean && echo "$dir : Ok" || echo "$dir : Nok";
+  make clean && echo -n "." || echo -e "$dir : \tNok";
   cd ..; 
 done
+cd libppm;
+make clean && echo -n "." || echo -e "libppm : \tNok";
+echo "";
+cd ..;
