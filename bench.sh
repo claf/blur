f7be3ac97dede0b816b12d98113ce06159ddaa40
Christophe Laferriere <christophe.laferriere@gmail.com>
2010-07-20 22:08:10 +0200

    Bench script (currently a reminder)

---
+++ bench.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "Starting Benchs";
+
+
+
+boucle 3 programmes
+
+boucles 50 executions
+
+boucles grains de 20 à 200
+
+execute programme avec grain avec num exec dans fichier.ppm
+diff fichier.ppm avec original
+sortie d'un programme des 50 exec dans le meme fichier
+
+
+
+awk sur les fichiers pour moyen dans fichier prog_grain
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+??? variation du nombre de threads?
+courbes gnuplot par rapport à seq
+
