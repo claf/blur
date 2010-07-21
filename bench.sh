52eab5ffd05382513dbfaa8920785dd124ee1be2
Christophe Laferriere <christophe.laferriere@gmail.com>
2010-07-21 11:36:14 +0200

    not working bench script

--- bench.sh
+++ bench.sh
@@ -2,21 +2,34 @@
 
 echo "Starting Benchs";
 
+BINARIES='blur_seq blur_adapt_xkaapi blur_adapt_xkaapi_half blur_dfg_xkaapi'
+IMG=/home/claferri/dev/blur/moon.ppm
 
+for prog in $BINARIES
+do
+    echo -n "Now benching $prog"
 
-boucle 3 programmes
+    if $prog eq 'blur_adapt_xkaapi_half' then
+	half_steal=1
+	echo " with half_steal on" 
+    else
+	half_steal=0
+	echo ""
+    fi
 
-boucles 50 executions
+    for execution in `seq 1 50`
+    do
+	echo "$execution for various block size :"
+	for block_size in `seq 20 10 200`
+	do
+	    echo -n "$block_size "
+	    #./$prog $IMG $IMG_$prog_$execution_$block_size.ppm $block_size $half_steal #redirection avec erreur!
+	    diff $IMG_$prog_$execution_$block_size.ppm $ORIGIMG || echo "File $IMG_$prog_$execution_$block_size.ppm differ from original!"
+	done
+    done
+done
 
-boucles grains de 20 à 200
-
-execute programme avec grain avec num exec dans fichier.ppm
-diff fichier.ppm avec original
-sortie d'un programme des 50 exec dans le meme fichier
-
-
-
-awk sur les fichiers pour moyen dans fichier prog_grain
+#awk sur les fichiers pour moyen dans fichier prog_grain
 
 
 
