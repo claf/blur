364bcab10e279117aae82229bdb385745ea0c3f0
Christophe Laferriere <christophe.laferriere@gmail.com>
2010-07-22 17:28:06 +0200

    work on bench script

--- bench.sh
+++ bench.sh
@@ -3,13 +3,19 @@
 echo "Starting Benchs";
 
 BINARIES='blur_seq blur_adapt_xkaapi blur_adapt_xkaapi_half blur_dfg_xkaapi'
-IMG=/home/claferri/dev/blur/moon.ppm
+IMG=moon
+ORIGIMG=/home/claferri/dev/blur/moon_blured.ppm
+DIR=/home/claferri/dev/blur/data/
+
+LD_LIBRARY_PATH=/home/claferri/opt/lib/
+
+cd bin
 
 for prog in $BINARIES
 do
     echo -n "Now benching $prog"
 
-    if $prog eq 'blur_adapt_xkaapi_half' then
+    if $prog eq 'blur_adapt_xkaapi_half'; then
 	half_steal=1
 	echo " with half_steal on" 
     else
@@ -23,11 +29,13 @@ do
 	for block_size in `seq 20 10 200`
 	do
 	    echo -n "$block_size "
-	    #./$prog $IMG $IMG_$prog_$execution_$block_size.ppm $block_size $half_steal #redirection avec erreur!
+	    ./$prog $IMG.ppm $DIR$IMG_$prog_$execution_$block_size.ppm $block_size $half_steal >> $DIR_Time_$prog_$block_size.txt
 	    diff $IMG_$prog_$execution_$block_size.ppm $ORIGIMG || echo "File $IMG_$prog_$execution_$block_size.ppm differ from original!"
 	done
     done
 done
+
+cd ../
 
 #awk sur les fichiers pour moyen dans fichier prog_grain
 
