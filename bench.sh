a741de13207297aa0240a93237d70751dee9c8df
Christophe Laferriere <christophe.laferriere@gmail.com>
2010-07-28 11:13:09 +0200

    added thread variation

--- bench.sh
+++ bench.sh
@@ -2,7 +2,7 @@
 
 echo "Starting Benchs";
 
-BINARIES='blur_adapt_xkaapi blur_adapt_xkaapi_half blur_dfg_xkaapi blur_seq'
+BINARIES='blur_adapt_xkaapi blur_adapt_xkaapi_half blur_dfg_xkaapi'
 #BINARIES='blur_seq'
 IMG=moon
 ORIGIMG=/home/claferri/dev/blur/moon_blured.ppm
@@ -16,40 +16,45 @@ LD_LIBRARY_PATH=/home/claferri/opt/lib/
 mkdir -p $DIR
 cd bin
 
-if [ `hostname` = "idkoiff" ]; then
-  echo "Numa Machine. Fixing sequential execution"
-  numa="numactl --physcpubind=0"
-else
-  numa=""
-fi
+# if [ `hostname` = "idkoiff" ]; then
+#   echo "Numa Machine. Fixing sequential execution"
+#   numa="numactl --physcpubind=0"
+# else
+#   numa=""
+# fi
 
-for prog in $BINARIES
+for thread in "1 2 4 8"
 do
-    echo -n "Now benching $prog"
+    for prog in $BINARIES
+    do
+	echo -n "Now benching $prog"
+	
+	if [ "$prog" = "blur_adapt_xkaapi_half" ]; then
+	    half_steal=1
+	    echo " with half_steal on" 
+	else
+	    half_steal=0
+	    echo ""
+	fi
 
-    if [ "$prog" = "blur_adapt_xkaapi_half" ]; then
-	half_steal=1
-	echo " with half_steal on" 
-    else
-	half_steal=0
-	echo ""
 	if [ "$prog" = "blur_seq" ]; then
 	    beg=$numa
 	else
 	    beg=""
+	    KAAPI_CPUCOUNT=$thread
 	fi
-    fi
-
-    for execution in `seq 1 50`
-    do
-	echo "$execution for various block size :"
-	for block_size in `seq 20 20 200`
+	
+	for execution in `seq 1 50`
 	do
-	    echo -n "$block_size "
-	    $beg ./$prog ../$IMG.ppm $DIR$IMG"_"$prog"_"$execution"_"$block_size.ppm $block_size $half_steal > tmp.txt
-	    diff $DIR$IMG"_"$prog"_"$execution"_"$block_size.ppm $ORIGIMG && cat tmp.txt >> $DIR"Time_"$prog"_"$block_size.txt || echo "File $IMG"_"$prog"_"$execution"_"$block_size.ppm differ from original!"
+	    echo "$execution for various block size :"
+	    for block_size in `seq 20 20 200`
+	    do
+		echo -n "$block_size "
+		LD_LIBRARY_PATH=$HOME/opt/lib/ $beg ./$prog ../$IMG.ppm $DIR$IMG"_"$prog"_"$execution"_"$block_size.ppm $block_size $half_steal > tmp.txt
+		diff $DIR$IMG"_"$prog"_"$execution"_"$block_size.ppm $ORIGIMG && cat tmp.txt >> $DIR"Time_"$prog"_"$block_size"_nbthread_"$thread.txt || echo "File $IMG"_"$prog"_"$execution"_"$block_size.ppm differ from original!"
+	    done
+	    echo ""
 	done
-    echo ""
     done
 done
 
