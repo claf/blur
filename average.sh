79c1f4e1685ac0ec56d015b74ea735c0dd92915f
Christophe Laferriere <christophe.laferriere@gmail.com>
2010-07-29 15:00:24 +0200

    Now average script create .data file containing every binaries time (avg, min and max) in a single per thread file

--- average.sh
+++ average.sh
@@ -1,11 +1,46 @@
 #!/bin/bash
 
-BINARIES="blur_seq blur_dfg_xkaapi blur_adapt_xkaapi blur_adapt_xkaapi_half"
+# Sequential time is treated differently :
+BINSEQ="blur_seq"
+BINARIES="blur_dfg_xkaapi blur_adapt_xkaapi blur_adapt_xkaapi_half"
 
-for prog in $BINARIES; do
-  echo "# $prog :" > $prog.data
-  for i in `seq 20 20 200`; do
-    echo -n "$i " >> $prog.data
-    awk '/total/ {s=s+$2;d=d+1} END {print s/d}' "Time_"$prog"_"$i.txt >> $prog.data
-  done
+FILENAME="BenchTime"
+
+for thread in 1 2 4 8 16; do
+
+    # file header :
+    echo -ne "#grain\t$BINSEQ avg\tmin\tmax" > $FILENAME"_nbthread_"$thread".data" 
+    for prog in $BINARIES;do
+	echo -ne "\t$prog avg\tmin\tmax" >> $FILENAME"_nbthread_"$thread".data" 
+    done
+    # next line :
+    echo "">> $FILENAME"_nbthread_"$thread".data" 
+
+    for grain in `seq 20 20 200`; do
+        # grain :
+	echo -ne "$grain" >> $FILENAME"_nbthread_"$thread".data" 
+
+	# sequential time :
+	avg=`cat "Time_"$BINSEQ"_"$grain.txt | grep total | awk '{s=s+$2;d=d+1} END {print s/d}'`
+	min=`cat "Time_"$BINSEQ"_"$grain.txt | grep total | awk '{print $2}' | sort -n | head -n 1`
+	max=`cat "Time_"$BINSEQ"_"$grain.txt | grep total | awk '{print $2}' | sort -nr | head -n 1`
+	echo -ne "\t$avg\t$min\t$max" >> $FILENAME"_nbthread_"$thread".data" 
+
+	for prog in $BINARIES; do
+	    # prog average :
+	    avg=`cat "Time_"$prog"_"$grain"_nbthread_"$thread.txt | grep total | awk '{s=s+$2;d=d+1} END {print s/d}'`
+
+	    # prog min :
+	    min=`cat "Time_"$prog"_"$grain"_nbthread_"$thread.txt | grep total | awk '{print $2}' | sort -n | head -n 1`
+
+	    # prog max :
+	    max=`cat "Time_"$prog"_"$grain"_nbthread_"$thread.txt | grep total | awk '{print $2}' | sort -nr | head -n 1`
+
+	    echo -ne "\t$avg\t$min\t$max" >> $FILENAME"_nbthread_"$thread".data" 
+	done
+
+	# next line :
+	echo "" >> $FILENAME"_nbthread_"$thread".data" 
+
+    done
 done
